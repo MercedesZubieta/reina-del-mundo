a069b40d1f2aac54b02176d5639312dc1b4fd61f
Mercedes Zubieta <mercedeszubieta@MacBook-Pro-de-Mercedes.local>
2019-04-12 20:34:14 -0500

    Upload folder

---
+++ RMBV-2/js/slider.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+		      $('.siema').slick({
+		        slidesToShow: 8,
+  				slidesToScroll: 6,
+		       	dots: false,
+			    prevArrow: false,
+			    nextArrow: false
+		      });
+                  $('.wrapper-lista-carusel-li').slick({
+                    slidesToShow: 8,
+                        slidesToScroll: 6,
+                        dots: false,
+                      prevArrow: false,
+                      nextArrow: false
+                  });
+
+
+
+		 });
